2e1c2b87974130bccfb878cc46b1cc908c357f49
Clecio Ferreira <clecioanderson@gmail.com>
2020-03-02 21:53:21 -0300

    new bash script

---
+++ install_flask_app.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+git clone https://github.com/clecioanderson/smplcrud.git
+virtualenv smplcrud
+source smplcrud/bin/activate
+cd smplcrud/
+pip install -r requirements.txt
+flask db init
+flask db migrate
+flask db upgrade
+sudo python app.py
