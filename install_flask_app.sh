9d33845660c073c92e4e5e970acec96706be4c20
Clecio Ferreira <clecioanderson@gmail.com>
2020-03-02 22:37:50 -0300

    no virtualenv changed

--- install_flask_app.sh
+++ install_flask_app.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 sudo yum -y install git
 git clone https://github.com/clecioanderson/smplcrud.git
-virtualenv smplcrud
-source smplcrud/bin/activate
 cd smplcrud/
-pip install -r requirements.txt
+sudo pip install -r requirements.txt
 flask db init
 flask db migrate
 flask db upgrade
-sudo python app.py
+sudo nohup python app.py
